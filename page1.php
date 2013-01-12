74b5e3afb2979dc442240c53cd40515e91583651
Md Shahnur Arefin <msn_arefin@yahoo.co.uk>
2013-01-12 14:34:05 +0600

    page1 and page2 added for url testing

---
+++ page1.php
@@ -0,0 +1,10 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>page 1</title>
+</head>
+
+<body>
+<a href="page2.php">Go to Page 2</a>
+</body>
+</html>
---
+++ page2.php
@@ -0,0 +1,10 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>page 2</title>
+</head>
+
+<body>
+<a href="page2.php" target="_blank"></a>
+</body>
+</html>
