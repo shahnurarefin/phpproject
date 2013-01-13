8e670d7501ef05a8504d7ea2bc681092bd4c474b
Md Shahnur Arefin <msn_arefin@yahoo.co.uk>
2013-01-13 22:18:55 +0600

    delete all data to initial

--- database.php
+++ database.php
@@ -1,32 +1,7 @@
 <?php 
-	//create database connection
-	$connection = mysql_connect("localhost","root","");
-	if(!$connection){
-		die("Database is not connected yet: " .  mysql_error());
-	}
-	// connect with database
+
 	
-	$db_connect = mysql_select_db("widget_corp",$connection);
-	
-	if(!$db_connect){
-		die("Database is not connected: " . mysql_error());
-	}
-	
-	//query result
-	
-	$result = mysql_query("select * from subjects",$connection);
-	
-	if(!$result){
-		die("There is not query: " . mysql_error());
-	}
-	
-	// result show
-	echo "<ul>";
-	while ($row = mysql_fetch_array($result)){
-		echo "<li>"; echo $row["menu_name"] . " " . $row["position"];echo "</li>";
-		
-	}
-	echo "</ul>";
+
 		
 ?>
 
@@ -37,9 +12,11 @@
 </head>
 
 <body>
+
 <?php 
- 
+
 ?>
+
 
 </body>
 </html>
