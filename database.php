cb4ccc5850af85d5b429e12ea2660a1924a43ddd
Md Shahnur Arefin <msn_arefin@yahoo.co.uk>
2013-01-13 20:53:40 +0600

    database initial file

---
+++ database.php
@@ -0,0 +1,25 @@
+<?php 
+	//create database connection
+	$connection = mysql_connect("localhost","root","");
+	if(!$connection){
+		die("Database is not connected yet: " .  mysql_error());
+	} else {
+		echo "Database connected successfully !!";
+	}
+		
+		
+?>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Form</title>
+</head>
+
+<body>
+<?php 
+ 
+?>
+
+</body>
+</html>
