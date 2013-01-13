b167614fef0ffadce799dc7213155c4a389a33d9
Md Shahnur Arefin <msn_arefin@yahoo.co.uk>
2013-01-13 22:13:59 +0600

    fetching data

--- database.php
+++ database.php
@@ -11,6 +11,22 @@
 	if(!$db_connect){
 		die("Database is not connected: " . mysql_error());
 	}
+	
+	//query result
+	
+	$result = mysql_query("select * from subjects",$connection);
+	
+	if(!$result){
+		die("There is not query: " . mysql_error());
+	}
+	
+	// result show
+	echo "<ul>";
+	while ($row = mysql_fetch_array($result)){
+		echo "<li>"; echo $row["menu_name"] . " " . $row["position"];echo "</li>";
+		
+	}
+	echo "</ul>";
 		
 ?>
 
