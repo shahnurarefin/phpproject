94d1673d9c4f041816609d1ebf2bebb88aa6f2c6
Md Shahnur Arefin <msn_arefin@yahoo.co.uk>
2013-01-14 15:49:29 -0800

    testing again

--- database.php
+++ database.php
@@ -1,17 +1,5 @@
 <?php 
-	// create connection
-	
-	$connection = mysql_connect("localhost","root","");
-	if(!$connection){
-			die("Database connection is not establised" . mysql_error());
-		}
-		
-	// selecting database
-	
-	$db_select = mysql_select_db("widget_corp",$connection);
-	if(!$db_select){
-		die("Database not found" .  mysql_error());
-		}
+
 		
 
 	
@@ -27,19 +15,7 @@
 
 <body>
 <?php 
-	// database query
-	
-	$result = mysql_query("SELECT * FROM subjects where id=3",$connection);
-	
-	if(!$result){
-		die("No data found in database" . mysql_error());
-		}
-		
-	// fetching data
-	
-	while($row = mysql_fetch_array($result)){
-			echo $row["id"] . " " . $row["menu_name"] . " " . $row["position"];	
-			echo "<br/>";
+
 									}
 
 ?>
